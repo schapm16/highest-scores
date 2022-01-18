78d026c7e03a56b210ea79c58c84d2f2f085a8e1
Stephen Chapman <Schapm16@gmail.com>
2022-01-18 15:16:54 -0500

    Create file readstream and initial parsing

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const readAndParseFile = require('./readAndParseFile');
+
 const { filePath, count } = require('./cli');
 
+readAndParseFile(filePath, count).then(data => console.log(data))
